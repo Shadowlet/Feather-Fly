d606f003d1c4b17efc539436c8b9de5f2135d696
98345 <zacharycarreiro@gmail.com>
2018-12-13 17:03:47 -0500

    Stuff

---
+++ Feather Fly/Assets/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    GameObject mainCamera;
+    GameObject feather;
+    float XVelocity = 0;
+    float YVelocity = 0;
+    float maxSpeed = 0;
+    bool fanOn;
+    bool featherFlying;
+    int currentFan;
+    
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        // Check key presses for fans every frame
+        if (Input.GetKey("A"))
+        {
+            fanLeft(); // Run fan logic
+        }
+        else if (Input.GetKey("S"))
+        {
+            fanBottom(); // Run fan logic
+        }
+        else if (Input.GetKey("D"))
+        {
+            fanRight(); // Run fan logic
+        }
+
+
+
+    }
+
+
+    private void fanLeft()
+    {
+        fanOn = true;
+        // Logic for the left fan blowing  
+    }
+
+    private void fanBottom()
+    {
+        fanOn = true;
+        // Logic for the bottom fan blowing
+    }
+
+    private void fanRight()
+    {
+        fanOn = true;
+        // Logic for the right fan blowing
+    }
+
+
+    public void gameOver(int status)
+    {
+        // collision stuff
+    }
+
+}
---
+++ Thing/Assets/Waypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+	private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(this.transform.position, 1);
+    }
+}
