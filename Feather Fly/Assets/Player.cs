b6689178b9c516daadb1f9ecd11f8903559b8360
98345 <zacharycarreiro@gmail.com>
2018-12-14 17:00:47 -0500

    Feather Fly

--- Feather Fly/Assets/Player.cs
+++ Feather Fly/Assets/Player.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+
+//-------NOTE TO SELF!!!!---------//
+
+// * Remove velocity garbage
 public class Player : MonoBehaviour {
-    GameObject mainCamera;
-    GameObject feather;
-    float XVelocity = 0;
-    float YVelocity = 0;
-    float maxSpeed = 0;
-    bool fanOn;
+    //public GameManager gameManager;
+    public Camera featherCamera;
+    public Rigidbody feather;
+    private float XVelocity = 0;
+    private float YVelocity = 0;
+    private float maxSpeedRight = 4;
+    private float maxSpeedLeft = -8;
+    bool fanOn = false;
     bool featherFlying;
-    int currentFan;
     
 
 
@@ -24,17 +30,43 @@ public class Player : MonoBehaviour {
     void Update() {
         
         // Check key presses for fans every frame
-        if (Input.GetKey("A"))
+        if (Input.GetKey(KeyCode.A))
         {
+            fanOn = true;
+        
             fanLeft(); // Run fan logic
+            
+            if(Mathf.Abs(XVelocity) > maxSpeedRight)
+            {
+                XVelocity = maxSpeedRight -= 1;
+
+            }
         }
-        else if (Input.GetKey("S"))
+        else if (Input.GetKey(KeyCode.S))
         {
+            
+            fanOn = true;
             fanBottom(); // Run fan logic
+            //feather.velocity = (new Vector3(XVelocity, YVelocity, 1));
+
+            
         }
-        else if (Input.GetKey("D"))
+        else if (Input.GetKey(KeyCode.D))
         {
+            fanOn = true;
             fanRight(); // Run fan logic
+
+            if (Mathf.Abs(XVelocity) > maxSpeedLeft)
+            {
+                XVelocity = maxSpeedLeft += 1;
+
+            }
+        }
+
+        else
+        {
+            fanOn = false;
+            Debug.Log(fanOn);
         }
 
 
@@ -44,22 +76,60 @@ public class Player : MonoBehaviour {
 
     private void fanLeft()
     {
-        fanOn = true;
         // Logic for the left fan blowing  
+
+        if (fanOn == true)
+        {
+            feather.AddForce(Vector3.right * 750);
+        }
+        else
+        {
+            fanOn = false; // turn off fan
+        }
+
     }
 
     private void fanBottom()
     {
         fanOn = true;
         // Logic for the bottom fan blowing
+
+        if (fanOn == true)
+        {
+            feather.AddForce(Vector3.up * 1000); // <----
+
+           
+
+        }
+        else
+        {
+            fanOn = false; // turn off fan
+        }
+
     }
 
     private void fanRight()
     {
         fanOn = true;
         // Logic for the right fan blowing
+
+        if(fanOn == true)
+        {
+
+            feather.AddForce(Vector3.left * 750);
+
+        }
+        else
+        {
+            fanOn = false; // turn off fan
+        }
+
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.gameObject.tag)
+    }
 
     public void gameOver(int status)
     {
